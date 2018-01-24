52bdc1b37b40e099b14fdf79dd41da2694a9742d
KacperLossa <kacper.lossa1234@gmail.com>
2018-01-24 09:25:53 +0100

    teb

---
+++ teb/JS/3_typy danych.js
@@ -0,0 +1,80 @@
+/*var a = 10, b = "5a";
+var wynik = a + b;
+
+//alert(wynik)
+alert(a+b);
+
+console.log("tekst");
+console.log(a); //10
+console.log(typeof(a)); //number
+console.log(typeof(b)); //string
+console.log(typeof(wynik)); //string
+
+console.trace(); 
+
+var _zmienna1 = 10;
+
+// var 3liczba = 10; błędna nazwa zmiennej
+
+var zmiennaZmiennoprzecikowa = 2.5; 
+console.log(zmiennaZmiennoprzecikowa);
+console.log(typeof(zmiennaZmiennoprzecikowa));
+
+//bool
+
+var prawda = true;
+console.log(typeof(prawda));
+
+//string
+
+var teks = "Kacper";
+console.log(typeof(tekst)); //string
+
+//undefined
+
+var inna;
+console.log(inna); //undefined
+console.log(typeof(inna)); //undefined*/
+
+//object
+
+/*var tab = ["aqua", "brązowy"];
+console.log(tab);
+console.log(typeof(tab)); //object
+
+//############################################################################
+
+//var liczba = prompt("Podaj liczbę");
+var liczba1 = prompt("Podaj liczbę", "liczba 1");
+var liczba2 = prompt("Podaj liczbę", "liczba 2");
+var suma = liczba1 + liczba2; //konkatenacja
+console.log(suma);
+
+//liczba1 = parseInt(liczba1); //konwersja na typ całkowity
+//liczba2 = parseInt(liczba2); //konwersja na typ całkowity
+// konmwersja na tyyp zmiennoprzecinkowy
+liczba1 = parseFloat(liczba1);
+liczba2 = parseFloat(liczba2);
+
+var suma1= liczba1 + liczba2;
+console.log("Po konwersji typów: " + suma1);
+
+console.log(typeof(liczba1)); //number*/
+/*
+console.log(typeof(lizcba2)); //number
+*/
+
+//zad.1
+
+/*użytkownik podaje z klawiarury długość boku kwadratu
+wyświetl na ekranie w formacie "Długość boku wynosi:...... cm"
+"Pole kwadratu wynosi: ......... cm2"*/
+
+
+var liczba1 = prompt("Podaj długość boku w cm");
+var wynik = liczba1 * liczba1;
+alert(wynik);
+console.log(typeof(liczba1));
+console.log(typeof(iloczyn));
+
+
